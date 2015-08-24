5629cff3aea748952800864b90a2378ac48eea2a
chauncey <cchauncey@gmail.com>
2015-08-24 09:51:01 -0500

    Hooking up tests

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,3 @@
-import os
 import app
 import unittest
 
@@ -6,15 +5,14 @@ import unittest
 class ThereBeDocsTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.app = app.app.test_client()
 
     def tearDown(self):
         pass
 
     def test_index(self):
-        rdata = self.app.get('/')
-        assert 'Let there be Docs!' in rdata
-
+        rsp = self.app.get('/')
+        assert 'Let there be Docs!' in rsp.data
 
 
 if __name__ == '__main__':
